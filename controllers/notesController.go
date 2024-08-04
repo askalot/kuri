3cace129718f96cf0580f2be42d54b339d39d1d6
Arunan Skanthan <59402100+askalot@users.noreply.github.com>
2024-08-04 22:06:03 +1000

    Add application layout

--- controllers/notesController.go
+++ controllers/notesController.go
@@ -6,17 +6,18 @@ import (
 )
 
 const (
+	applicationLayout   = "views/layouts/application.html"
 	notesViewsDirectory = "views/notes/"
 )
 
 func NotesIndex(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles(notesViewsDirectory + "index.html"))
+	tmpl := template.Must(template.ParseFiles(applicationLayout, notesViewsDirectory+"index.html"))
 
 	tmpl.Execute(w, nil)
 }
 
 func NotesNew(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles(notesViewsDirectory + "new.html"))
+	tmpl := template.Must(template.ParseFiles(applicationLayout, notesViewsDirectory+"new.html"))
 
 	tmpl.Execute(w, nil)
 }
