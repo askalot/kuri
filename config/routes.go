12b0f205eeb6077a8bfc3d94d3da72afa75f43db
Arunan Skanthan <59402100+askalot@users.noreply.github.com>
2024-08-04 21:43:49 +1000

    Use `chi` Logger middleware

--- config/routes.go
+++ config/routes.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/askalot/kuri/controllers"
 )
@@ -13,5 +14,7 @@ const (
 )
 
 func SetupRoutes() {
+	Router.Use(middleware.Logger)
+
 	Router.Get(NotesIndex, controllers.NotesIndex)
 }
