9986ae2b492811780a632111b0b5666aee4b7443
Arunan Skanthan <59402100+askalot@users.noreply.github.com>
2024-08-05 16:07:45 +1000

    Cleanup setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+# Setup environment variables
 if [ ! -f ".env" ]; then
 	cp ".env.example" ".env";
 fi
 
+# Download Go modules
 if ! command -v go >/dev/null 2>&1; then
-	go mod tidy
 	go mod download
 fi
 
+# Install Air
 if ! command -v air >/dev/null 2>&1; then
 	go install github.com/air-verse/air@latest
 fi
