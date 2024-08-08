758080d37ea611afdae0023f5fd73b9dd6173a46
Arunan Skanthan <59402100+askalot@users.noreply.github.com>
2024-08-08 10:58:05 +1000

    Remove incorrect use of UTC to generate date, time string

--- internal/timeutils/timeutils.go
+++ internal/timeutils/timeutils.go
@@ -5,7 +5,7 @@ import "time"
 const goReferenceTime = "20060102T150405"
 
 func GenerateCurrentDateTimeString() string {
-	now := time.Now().UTC()
+	now := time.Now()
 
 	return now.Format(goReferenceTime)
 }
