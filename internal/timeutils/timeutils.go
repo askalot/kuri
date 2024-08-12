bd7b15965c35950b30aa382a99eec6cc21f3d916
Arunan Skanthan <59402100+askalot@users.noreply.github.com>
2024-08-12 18:53:25 +1000

    Clean up timeutils

--- internal/timeutils/timeutils.go
+++ internal/timeutils/timeutils.go
@@ -5,7 +5,5 @@ import "time"
 const goReferenceTime = "20060102T150405"
 
 func GenerateCurrentDateTimeString() string {
-	now := time.Now()
-
-	return now.Format(goReferenceTime)
+	return time.Now().Format(goReferenceTime)
 }
